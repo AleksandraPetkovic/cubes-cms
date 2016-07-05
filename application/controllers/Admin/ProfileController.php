1dc1694ea2f0cd087ddb0bad65c5de36e1b31da5
Aleksandra Petkovic <a13x.petkovic@gmail.com>
2016-07-05 18:03:31 +0200

    Domaci za 06.07.2016.

--- application/controllers/Admin/ProfileController.php
+++ application/controllers/Admin/ProfileController.php
@@ -5,7 +5,7 @@ class Admin_ProfileController extends Zend_Controller_Action{
         
     }
     
-    public function changepassword() {
+    public function changepasswordAction() {
         
     }
 }
