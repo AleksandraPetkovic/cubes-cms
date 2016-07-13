e9198f62371af8f2865df35ba872dd7fd5f03993
Aleksandra Petkovic <a13x.petkovic@gmail.com>
2016-07-13 20:21:12 +0200

    Cas 13.07.2016. JS

--- application/controllers/Admin/MembersController.php
+++ application/controllers/Admin/MembersController.php
@@ -118,7 +118,7 @@ class Admin_MembersController extends Zend_Controller_Action {
             'success' => $flashMessenger->getMessages('success'),
             'errors' => $flashMessenger->getMessages('errors'),
         );
-           //forma sluzi za filtriranje i validaciju polja
+        //forma sluzi za filtriranje i validaciju polja
         $form = new Application_Form_Admin_MemberAdd();
 
         //default form data
@@ -165,9 +165,209 @@ class Admin_MembersController extends Zend_Controller_Action {
 
         $this->view->systemMessages = $systemMessages;
         $this->view->form = $form;
-        
+
 
         $this->view->member = $member;
     }
+
+    public function deleteAction() {
+
+        $request = $this->getRequest();
+
+        if (!$request->isPost() || $request->isPost('task') != 'delete') {
+            //request is not post redirect to index page
+            //redirect to same or another page
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+        }
+
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        try {
+            //(int) sve sto nije integer pretvara u nulu :)
+            //read $_POST['id']
+            $id = (int) $request->getPost('id');
+
+            if ($id <= 0) {
+
+
+                //prekida se izvrsavanje programa i prikazuje se "Page not found"
+                throw new Zend_Controller_Router_Exception('Invalid member id: ' . $id);
+            }
+
+            $cmsMembersTable = new Application_Model_DbTable_CmsMembers();
+
+            $member = $cmsMembersTable->getMemberById($id);
+
+
+            if (empty($member)) {
+
+                throw new Zend_Controller_Router_Exception('No member is found with id: ' . $id, 'errors');
+            }
+
+            $cmsMembersTable->deleteMember($id);
+
+
+            $flashMessenger->addMessage('Member ' . $member['first_name'] . ' ' . $member['last_name'] . 'has been deleted', 'success');
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+            
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+        }
+    }
+
+    
+    
+    public function disableAction() {
+
+        $request = $this->getRequest();
+
+        if (!$request->isPost() || $request->isPost('task') != 'disable') {
+            //request is not post redirect to index page
+            //redirect to same or another page
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+        }
+
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        try {
+            //(int) sve sto nije integer pretvara u nulu :)
+            //read $_POST['id']
+            $id = (int) $request->getPost('id');
+
+            if ($id <= 0) {
+
+
+                //prekida se izvrsavanje programa i prikazuje se "Page not found"
+                throw new Zend_Controller_Router_Exception('Invalid member id: ' . $id);
+            }
+
+            $cmsMembersTable = new Application_Model_DbTable_CmsMembers();
+
+            $member = $cmsMembersTable->getMemberById($id);
+
+
+            if (empty($member)) {
+
+                throw new Zend_Controller_Router_Exception('No member is found with id: ' . $id, 'errors');
+            }
+
+            $cmsMembersTable->disableMember($id);
+
+
+            $flashMessenger->addMessage('Member ' . $member['first_name'] . ' ' . $member['last_name'] . 'has been disabled', 'success');
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+            
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+        }
+    }
+    
+    
+    
+    public function enableAction() {
+
+        $request = $this->getRequest();
+
+        if (!$request->isPost() || $request->isPost('task') != 'enable') {
+            //request is not post redirect to index page
+            //redirect to same or another page
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+        }
+
+        $flashMessenger = $this->getHelper('FlashMessenger');
+
+        try {
+            //(int) sve sto nije integer pretvara u nulu :)
+            //read $_POST['id']
+            $id = (int) $request->getPost('id');
+
+            if ($id <= 0) {
+
+
+                //prekida se izvrsavanje programa i prikazuje se "Page not found"
+                throw new Zend_Controller_Router_Exception('Invalid member id: ' . $id);
+            }
+
+            $cmsMembersTable = new Application_Model_DbTable_CmsMembers();
+
+            $member = $cmsMembersTable->getMemberById($id);
+
+
+            if (empty($member)) {
+
+                throw new Zend_Controller_Router_Exception('No member is found with id: ' . $id, 'errors');
+            }
+
+            $cmsMembersTable->enableMember($id);
+
+
+            $flashMessenger->addMessage('Member ' . $member['first_name'] . ' ' . $member['last_name'] . 'has been enabled', 'success');
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+            
+        } catch (Application_Model_Exception_InvalidInput $ex) {
+            $flashMessenger->addMessage($ex->getMessage(), 'errors');
+
+            $redirector = $this->getHelper('Redirector');
+            $redirector->setExit(true)
+                    ->gotoRoute(array(
+                        'controller' => 'admin_members',
+                        //ako se ne stavi action onda se podrazumeva index, ovo je stavljeno radi jasnoce :)
+                        'action' => 'index',
+                            ), 'default', true);
+        }
+    }
+
 
 }
